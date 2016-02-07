969cc9bcd3c597c748bef413df8c5aa60bd00139
shad0wfox <l45632100@hotmail.com>
2016-02-08 02:39:49 +0800

    login and sign up link

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,10 +5,11 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
-
+	
 	def create
 		@user = User.new( user_params )
 		if @user.save
+			session[:user_id] = @user.id
 			redirect_to root_path
 		else
 			render 'new'
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,9 @@ Rails.application.routes.draw do
   get 'signup' => 'users#new'
   resources :users
 
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   root  'products#index'
   # The priority is based upon order of creation: first created -> highest priority.
